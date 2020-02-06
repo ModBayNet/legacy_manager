8b2a4deb11d1df4979d6a41c437e452f04b38e9f
Eugene <gish.ee18@gmail.com>
2020-02-06 19:53:27 +0300

    Add pause/unpause methods for tasks

--- mb_manager/tasks/task.py
+++ mb_manager/tasks/task.py
@@ -15,10 +15,13 @@ class Task(type):
     def __init__(cls: type, name: str, bases: Tuple[type, ...], dct: Dict[str, Any]):
         for base in cls.__mro__:
             if "interval" in base.__dict__:
-                cls._instances.add(cls())  # type: ignore
+                instance = cls()
+                cls._instance = instance
+                cls._instances.add(instance)  # type: ignore
 
 
 class BaseTask(metaclass=Task):
+    _instance: BaseTask
     _instances: Set[BaseTask] = set()
 
     interval: int
@@ -27,11 +30,31 @@ class BaseTask(metaclass=Task):
         self._task: Optional[asyncio.Task[None]] = None
         self._cancelled = False
 
+        # cannot be set here because aiohttp starts a thread that breaks asyncio
+        self._unpaused: asyncio.Event
+
     async def setup(self, app: web.Application) -> None:
-        pass
+        self._unpaused = asyncio.Event()
+        self._unpaused.set()
 
     async def stop(self) -> None:
         pass
+
+    @classmethod
+    def _get_instance(cls) -> BaseTask:
+        return cls._instance
+
+    @classmethod
+    def pause(cls) -> None:
+        log.debug("task %s paused", cls.__name__)
+
+        cls._get_instance()._unpaused.clear()
+
+    @classmethod
+    def unpause(cls) -> None:
+        log.debug("task %s unpaused", cls.__name__)
+
+        cls._get_instance()._unpaused.set()
 
     @staticmethod
     async def schedule_all(app: web.Application) -> None:
@@ -47,14 +70,17 @@ class BaseTask(metaclass=Task):
 
     async def _run_forever(self) -> None:
         while not self._cancelled:
-            log.debug("running task %s", self.__class__.__name__)
+            # sleeping before task gives supervisor tasks small buffer of time that lets
+            # them not flood Sentry with false errors while backend is booting
+            await asyncio.sleep(self.interval)
 
+            await self._unpaused.wait()
+
+            log.debug("running task %s", self.__class__.__name__)
             try:
                 await self.run_once()
             except Exception:
                 log.exception("error running task")
-
-            await asyncio.sleep(self.interval)
 
     @abc.abstractmethod
     async def run_once(self) -> None:
